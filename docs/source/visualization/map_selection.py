a8ca37dcb1b1b04e20a43ac7a2e150107c1c9648
Aimee Barciauskas <aimee@developmentseed.org>
2022-10-16 11:02:38 -0700

    Tried with panel, aborted, now using voila

--- docs/source/visualization/map_selection.py
+++ docs/source/visualization/map_selection.py
@@ -1,25 +1,39 @@
-from email.policy import default
-import geoviews as gv
-import holoviews as hv
 import panel as pn
 import param
-from geoviews import opts, tile_sources as gvts
 import requests
 import json
-gv.extension('bokeh', logo=False)
-pn.extension()
+pn.extension("ipywidgets", sizing_mode="stretch_width")
+from ipyleaflet import Map, basemaps, basemap_to_tiles, DrawControl, TileLayer, LayersControl
 
 provider = 'NASA_MAAP'
 cmr_search_url = "https://cmr.maap-project.org/search"
 
 class Mapview(param.Parameterized):
-    tiles         = gvts.CartoLight.opts('WMTS', xaxis=None, yaxis=None, width=1200, height=700, global_extent=True)
-    box_polygons  = gv.Polygons([]).opts(fill_alpha=0.1)
-    box_colours   = ['red','blue','green','orange','purple']
-    box_stream    = hv.streams.BoxEdit(source=box_polygons, num_objects=1, styles={'fill_color': box_colours})
+    map = Map(
+        basemap=basemap_to_tiles(basemaps.OpenStreetMap.Mapnik),
+        center=(0, 0),
+        zoom=4
+    )
+    draw_control = DrawControl()
+    draw_control.rectangle = {
+        "shapeOptions": {
+            "fillColor": "#fca45d",
+            "color": "#fca45d",
+            "fillOpacity": 0.2
+        }
+    }
+    map.add_control(draw_control)
+    geo_json = {}
+
+    def handle_draw(self, geo_json):
+        print('IN HANDLE DRAW')
+        self.geo_json = geo_json
+
+    draw_control.on_draw(handle_draw)    
 
     def show(self):
-        return self.tiles * self.box_polygons
+        print('IN SHOW')
+        return self.map
 
     def add_layer_to_map(self, s3_url):
         tititler_url = "https://l40l1vwmvi.execute-api.us-west-2.amazonaws.com"
@@ -27,10 +41,11 @@ class Mapview(param.Parameterized):
         colormap_name = "gist_earth_r"
         color_formula = "gamma r 2"
         #tititler_url = "https://titiler.maap-project.org"
-        tiles_url = tititler_url + "/cog/tiles/{Z}/{Y}/{X}.png?url=" + f"{s3_url}&rescale={rescale}&colormap_name={colormap_name}&color_formula={color_formula}"
+        tiles_url = tititler_url + "/cog/tiles/{z}/{x}/{y}.png?url=" + f"{s3_url}&rescale={rescale}&colormap_name={colormap_name}&color_formula={color_formula}"
         #tiles_url = tititler_url + "/cog/tiles/{z}/{x}/{y}.png?url=" + f"{granule_s3_url}&rescale=350,850&colormap_name={colormap_name}"
-        data_layer = gvts.WMTS(tiles_url)
-        return self.tiles * data_layer
+        tile_layer=TileLayer(url=tiles_url)
+        self.map.add_control(LayersControl())
+        return self.map.add_layer(tile_layer)
 
 class CollectionSelection(param.Parameterized):
     collections_search_url = f"{cmr_search_url}/collections.json?provider={provider}&page_size=100"
@@ -73,18 +88,22 @@ class Dashboard(param.Parameterized):
 
     @param.depends('update_geometries_button')
     def update_geometry(self):
-        box_data = self.mapview.box_stream.data or {}
-        llong = box_data.get('x0', [0.])[0]
-        llat =  box_data.get('y0', [0.])[0]
-        ulong = box_data.get('x1', [0.])[0]
-        ulat = box_data.get('y1', [0.])[0]
-        self.bbox = ','.join(map(str, [llong, llat, ulong, ulat]))
-        return pn.Row(
-            pn.widgets.FloatInput(name='Lower Longitude', value=llong),
-            pn.widgets.FloatInput(name='Lower Latitude', value=llat),
-            pn.widgets.FloatInput(name='Upper Longitude', value=ulong),
-            pn.widgets.FloatInput(name='Upper Latitude', value=ulat),
-        )
+        print('updating geometries')
+        print(self.mapview.draw_control.rectangle)
+        self.bbox = '0,0,0,0'
+        return pn.Row(pn.widgets.FloatInput(name='Lower Longitude', value=0))
+        # box_data = self.mapview.geo_json or {}
+        # llong = box_data.get('x0', [0.])[0]
+        # llat =  box_data.get('y0', [0.])[0]
+        # ulong = box_data.get('x1', [0.])[0]
+        # ulat = box_data.get('y1', [0.])[0]
+        # self.bbox = ','.join(map(str, [llong, llat, ulong, ulat]))
+        # return pn.Row(
+        #     pn.widgets.FloatInput(name='Lower Longitude', value=llong),
+        #     pn.widgets.FloatInput(name='Lower Latitude', value=llat),
+        #     pn.widgets.FloatInput(name='Upper Longitude', value=ulong),
+        #     pn.widgets.FloatInput(name='Upper Latitude', value=ulat),
+        # )
 
     @param.depends('granule_button') 
     def search_for_granules(self):
@@ -103,13 +122,27 @@ class Dashboard(param.Parameterized):
             pn.Column(
                 self.collection_selection,
                 self.param['update_geometries_button'],
-                self.update_geometry,
-                self.param['granule_button'],
-                self.search_for_granules,
-                self.param['add_layer_to_map_button'],
-                self.add_layer_to_map
+                self.update_geometry(),
+                self.component
+                # self.param['granule_button'],
+                # self.search_for_granules,
+                # self.param['add_layer_to_map_button'],
+                # self.add_layer_to_map
             )
         )
 
-d = Dashboard()
-d.view().show()
+# d = Dashboard()
+# d.view().show()
+
+map = Mapview().show()
+button = pn.widgets.Button(name='Click me', button_type='primary')
+text = pn.widgets.TextInput(value='Ready')
+
+def b(event):
+    text.value = 'Clicked {0} times'.format(button.clicks)
+    
+button.on_click(b)
+pn.Row(
+    pn.Column(button, text),
+    pn.Column(map)
+).servable()
